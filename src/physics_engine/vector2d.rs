80e1449659dd04a722be0aaa07164727d6d26e27
JatoMixo <jatomixo@outlook.es>
2025-04-16 15:51:50 +0200

    implemented add and add asign to Vector2d

--- src/physics_engine/vector2d.rs
+++ src/physics_engine/vector2d.rs
@@ -21,12 +21,30 @@ impl Sum for Vector2D {
     }
 }
 
+impl std::ops::Add for Vector2D {
+    type Output = Vector2D;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector2D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vector2D {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
 #[cfg(test)]
 mod vector2d_tests {
     use super::*;
 
     #[test]
-    fn test_sum() {
+    fn test_iterator_sum() {
         let vectors: Vec<Vector2D> = [
             Vector2D { x: 6f64, y: 4f64 },
             Vector2D { x: 2f64, y: 3f64 },
@@ -34,5 +52,23 @@ mod vector2d_tests {
         ].to_vec();
 
         assert_eq!(Vector2D { x: 19f64, y: 22f64 }, vectors.into_iter().sum());
+    }
+
+    #[test]
+    fn test_add() {
+        let vector_0 = Vector2D { x: 4f64, y: 5f64 };
+        let vector_1 = Vector2D { x: 11f64, y: 2f64 };
+
+        assert_eq!(Vector2D { x: 15f64, y: 7f64 }, vector_0 + vector_1);
+    }
+
+    #[test]
+    fn test_add_assign() {
+        let mut vector_0 = Vector2D { x: 4f64, y: 5f64 };
+        let vector_1 = Vector2D { x: 11f64, y: 2f64 };
+
+        vector_0 += vector_1;
+
+        assert_eq!(Vector2D { x: 15f64, y: 7f64 }, vector_0);
     }
 }
